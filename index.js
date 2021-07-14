06f753bd9b0127527e86a8453ca1c2a499b02acd
vmamidi <vmamidi@expedia.com>
2021-07-14 15:39:23 -0700

    customizing name color with input

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const styles = require('ansi-styles');
 
 try {
 
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello \u001b[48;5;6m ${nameToGreet}!`);
+  const nameColorHex = "#" + core.getInput('hex-color-code-for-name');
+  console.log("Hello " + styles.color.ansi16m(...styles.hexToRgb(nameColorHex)) + nameToGreet + styles.color.close + "!");
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
