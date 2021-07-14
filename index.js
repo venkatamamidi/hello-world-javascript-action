c752c114a3fcd8b860b11804fa0b04c51d559400
vmamidi <vmamidi@expedia.com>
2021-07-14 16:13:25 -0700

    reset to v1.1 and just adding ansi-styles module

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const style = require('ansi-styles');
 
 try {
 
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello \u001b[48;5;6m ${nameToGreet}!`);
+  console.log("Hello" + style.color.ansi16m.hex('#abcdef') +  nameToGreet + "!");
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
